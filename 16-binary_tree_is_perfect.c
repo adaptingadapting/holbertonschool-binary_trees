32f14208b6b7afaba7e7b11aaef887c77d52fa71
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-11 16:13:20 -0300

    revamped task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,31 +10,25 @@ size_t binary_tree_height(const binary_tree_t *tree);
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);	
-	if (binary_tree_height(tree->left) &&  !binary_tree_height(tree->right))
-		return (1);
-  	return (binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right));
-	return (0);
+	return (binary_tree_is_full(tree) && !binary_tree_balance(tree));
 }
 
 /**
- * binary_tree_height - returns the height of a binary tree
- * @tree: pointer to node to measure the hgiht of
- * Return: height of tree or 0
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: tree to check
+ * Return: 1 if full 0 if not
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t height_1 = 0;
-	size_t height_2 = 0;
-
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
-	height_1 = 1 + binary_tree_height(tree->left);
-	height_2 = 1 + binary_tree_height(tree->right);
-	if (height_1 > height_2)
-		return (height_1);
-	return (height_2);
+	if ((tree->left) && !(tree->right) || tree->right && !tree->left)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
 }
