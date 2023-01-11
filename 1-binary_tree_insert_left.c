68b83d888f841d15dfbef396ca4abe13bc766f86
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-11 11:34:57 -0300

    task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node to the left
+ * @parent: parent of the new node
+ * @value: value to insert
+ * Return: returns a pointer to the new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int n)
+{
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->right = NULL;
+	new_node->left = NULL;
+	new_node->parent = parent;
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+		parent->left = new_node;
+	}
+	else
+		parent->left = new_node;
+	return (new_node);
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 readme for the binary trees repo
 
 task 0 is trivial, its going to be put to good use later tho with some other functions
+
+task 1 again was rather easy, i have to say my experience with the speedrun project has definetively been a plus as now im much more experienced with the "nuances" of linked lists and similar data structures
