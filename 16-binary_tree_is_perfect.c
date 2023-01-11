9e70b03d20df9df762030bc4a7b105bee0fb0b6a
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-11 16:22:26 -0300

    hotfix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 
-size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_perfect - checks for the perfection of a tree
@@ -32,4 +34,39 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 	return (0);
+}
+
+size_t binary_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_balance - returns the balance of the tree
+ * @tree: tree to parse through
+ * Return: the height of the left subtree minus the height of the right
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_height(tree->left) - binary_height(tree->right));
+}
+
+/**
+ * binary_height - returns the height of a binary tree
+ * @tree: pointer to node to measure the hgiht of
+ * Return: height of tree or 0
+ */
+
+size_t binary_height(const binary_tree_t *tree)
+{
+	size_t height_1 = 0;
+	size_t height_2 = 0;
+
+	if (!tree)
+		return (0);
+	height_1 = 1 + binary_height(tree->left);
+	height_2 = 1 + binary_height(tree->right);
+	if (height_1 > height_2)
+		return (height_1);
+	return (height_2);
 }
