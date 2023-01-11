718721bab06b9eefcbaae8f6651936a654c21a9f
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-11 16:18:40 -0300

    hotfix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-
+	if (((tree->left) && !(tree->right)) ||
+	    ((tree->right) && !(tree->left)))
+		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
 	if (tree->left && tree->right)
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,7 +23,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if ((tree->left) && !(tree->right) || tree->right && !tree->left)
+	if (((tree->left) && !(tree->right)) ||
+		(tree->right && !(tree->left)))
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
