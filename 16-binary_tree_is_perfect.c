88f7275955d7efa45ec75d20bcc51306ec75782f
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-11 15:09:11 -0300

    hotfix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
-size_t binary_height(const binary_tree_t *tree)
-int binary_tree_balance(const binary_tree_t *tree)
-int binary_tree_is_full(const binary_tree_t *tree)
+size_t binary_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
   
 /**
  * binary_tree_is_perfect - checks for the perfection of a tree
