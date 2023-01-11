94b0de68ba91ac6494f4a8c28ec617b743571ffa
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-11 14:16:32 -0300

    task 14 revised

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,12 +10,13 @@ size_t binary_height(const binary_tree_t *tree);
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	return (binary_height(tree->left)
-		- binary_height(tree->right));
+	if (!tree)
+		return (0);
+	return (binary_height(tree->left) -binary_height(tree->right));
 }
 
 /**
- * binary_height - returns the height of a binary tree
+ * binary_tree_height - returns the height of a binary tree
  * @tree: pointer to node to measure the hgiht of
  * Return: height of tree or 0
  */
@@ -27,30 +28,8 @@ size_t binary_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	height_1 = 1 + binary_tree_height(tree->left);
-	height_2 = 1 + binary_tree_height(tree->right);
-	if (height_1 > height_2)
-		return (height_1);
-	return (height_2);
-}
-
-#include "binary_trees.h"
-
-/**
- * binary_tree_height - returns the height of a binary tree
- * @tree: pointer to node to measure the hgiht of
- * Return: height of tree or 0
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height_1 = 0;
-	size_t height_2 = 0;
-
-	if (!tree || (!tree->left && !tree->right))
-		return (0);
-	height_1 = 1 + binary_tree_height(tree->left);
-	height_2 = 1 + binary_tree_height(tree->right);
+	height_1 = 1 + binary_height(tree->left);
+	height_2 = 1 + binary_height(tree->right);
 	if (height_1 > height_2)
 		return (height_1);
 	return (height_2);
