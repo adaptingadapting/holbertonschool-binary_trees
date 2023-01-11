42f19d5b56b7bd252e2a28bca5f485e34732b616
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-11 17:06:45 -0300

    changed name

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a binary t node
+ * @node: node to find sibling from
+ * Return: returns the sibling a pointer to it i should say
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+	{
+		if (node->parent->right)
+			return (node->parent->right);
+		return (NULL);
+	}
+	if (node->parent->left != node)
+	{
+		if (node->parent->left)
+			return (node->parent->left);
+		return (NULL);
+	}
+	return (NULL);
+}
